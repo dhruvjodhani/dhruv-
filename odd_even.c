de69168e061f51ccb32cb2aef77738519721603a
dhruvjodhani <jodhanidhruv2@gmail.com>
2024-10-02 21:47:14 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int n;
+    scanf("%d",&n);
+    if(n&1 )
+    {
+        printf("number is odd ");
+    }
+    else{
+        printf("number is even");
+    }
+}
